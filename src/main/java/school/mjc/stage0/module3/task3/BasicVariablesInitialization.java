78cffe3e5494b6c6aba82c59aec71a93e3fa9bcc
BigVito7 <biggvito7@gmail.com>
2025-02-25 11:41:43 +0200

    Reassigning values, new for me

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -1,7 +1,14 @@
 package school.mjc.stage0.module3.task3;
 
+import school.mjc.parser.predicate.SoutPredicate;
+
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println("First: " + first +"\n"
+                        + "Second: " + second + "\n"
+                        + "Third: " + third);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println("First: " + first +"\n"
+                + "Second: " + second + "\n"
+                + "Third: " + third);
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.println("First: " + first);
+        System.out.println("Second: " + second);
+        System.out.println("Third: " + third);
+        System.out.println("LinkToFirst: " + linkToFirst);
+        System.out.println("LinkToSecond: " + linkToSecond);
+        System.out.println("LinkToThird: " + linkToThird);
     }
 }
